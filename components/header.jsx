ea6afe7f4cab830b8588fa385df3dd5e7f884974
Fardeem Munir <fardeem.munir@erubai.com>
2019-10-20 16:55:00 -0500

    Add more uniform padding to the header component

--- components/header.jsx
+++ components/header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => (
-  <header className="pt-8 text-white">
+  <header className="py-8 text-white">
     <div className="container flex justify-between items-center">
       <h1 className="uppercase text-xl tracking-widest">Gameplan</h1>
       <h2 className="text-gray-300 text-xs italic opacity-75">
