3cdcec90d709c16555db1fab724aeb6a50fda711
Fardeem Munir <fardeem.munir@erubai.com>
2019-10-20 16:22:42 -0500

    Make a header component and include it in the homepage

---
+++ components/header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => (
+  <header className="pt-8 text-white">
+    <div className="container flex justify-between items-center">
+      <h1 className="uppercase text-xl tracking-widest">Gameplan</h1>
+      <h2 className="text-gray-300 text-xs italic opacity-75">
+        Design Wonderful Semesters
+      </h2>
+    </div>
+  </header>
+);
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,19 @@
 import React from "react";
 import Head from "next/head";
-import Nav from "../components/nav";
 
-import "../styles/index.css";
+import Header from "../components/header";
+
+import "../styles/main.css";
 
 const Home = () => (
-  <div>
+  <main>
     <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
+      <title>Gameplan – Design Wonderful Semesters</title>
+      <link rel="shortcut icon" href="/favicon.png" />
     </Head>
 
-    <div className="p-4 shadow rounded bg-white">
-      <h1 className="text-purple-500 leading-normal">Next.js</h1>
-      <p className="text-gray-500">with Tailwind CSS</p>
-    </div>
-  </div>
+    <Header />
+  </main>
 );
 
 export default Home;
