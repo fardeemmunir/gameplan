63a9e8f5135eae499e90906646af62da173874ad
Fardeem Munir <fardeem.munir@erubai.com>
2019-10-26 16:50:04 -0500

    Add difficulty and quarter pref. to the class card

--- pages/index.jsx
+++ pages/index.jsx
@@ -22,15 +22,28 @@ const Home = () => (
     </div>
 
     <div className="flex justify-center">
-      <div className="w-40 h-24 rounded p-2 bg-purple-500 shadow relative">
-        <h2 className="font-bold text-xs tracking-widest font-mono mb-2">
-          ES_APPM 252-1
-        </h2>
-        <h1>Integral Calculus</h1>
+      <div className="w-40 h-24 rounded p-2 bg-purple-500 shadow flex flex-col justify-between">
+        <header>
+          <h2 className="font-bold text-xs tracking-widest font-mono">
+            ES_APPM 252-1
+          </h2>
+          <h1>Integral Calculus</h1>
+        </header>
 
-        <div className="absolute bottom-0 right-0 text-xs p-1 bg-purple-600 hover:bg-purple-600 leading-none rounded-br rounded-tl cursor-pointer">
-          Edit
-        </div>
+        <footer className="flex justify-between">
+          <div className="text-sm meta relative">
+            <span className="cursor-pointer">1 – FS</span>
+
+            <div className="bg-black  absolute meta__tooltip text-white border border-gray-800 border-l-4 w-32 p-1 text-xs">
+              <p>Difficulty: 1/5</p>
+              <p>Quarter: Fall/Spring</p>
+            </div>
+          </div>
+
+          <button className="text-xs p-1 bg-purple-600 hover:bg-purple-600 leading-none rounded cursor-pointer">
+            Edit
+          </button>
+        </footer>
       </div>
     </div>
   </main>
