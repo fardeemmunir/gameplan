ffc98d3d1adc05f9addb1e245629cab09d758b35
Fardeem Munir <fardeem.munir@erubai.com>
2019-10-27 03:15:38 -0500

    Switch up the colors

--- lib/difficultyToColor.tsx
+++ lib/difficultyToColor.tsx
@@ -1,15 +1,17 @@
+const colors = ["#25a18e", "#00a5b2", "#00a5cf", "#8b7bc5", "#bd4f6c"];
+
 function difficultyToColor(difficulty: number) {
   switch (difficulty) {
     case 1:
-      return "#C45AB3";
+      return colors[0];
     case 2:
-      return "#D0FCB3";
+      return colors[1];
     case 3:
-      return "#7D7ABC";
+      return colors[2];
     case 4:
-      return "#EF767A";
+      return colors[3];
     case 5:
-      return "#CC5A71";
+      return colors[4];
     default:
       return "#eee";
   }
