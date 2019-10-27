9d1dd3e104328d2303b697597177253c95266112
Fardeem Munir <fardeem.munir@erubai.com>
2019-10-26 21:40:38 -0500

    Stuff!

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,48 +6,29 @@ import Header from "../components/Header";
 import "../styles/main.css";
 import AddClassForm from "../components/AddClassForm";
 
-import "../store";
+import { StoreProvider } from "../lib/store";
+import ClassList from "../components/ClassList";
+import Graph from "../components/graph";
 
 const Home = () => (
-  <main>
-    <Head>
-      <title>Gameplan – Design Wonderful Semesters</title>
-      <link rel="shortcut icon" href="/favicon.png" />
-    </Head>
+  <StoreProvider>
+    <main>
+      <Head>
+        <title>Gameplan – Design Wonderful Semesters</title>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
 
-    <Header />
+      <Header />
 
-    <div className="container">
-      <AddClassForm />
-    </div>
-
-    <div className="flex justify-center">
-      <div className="w-40 h-24 rounded p-2 bg-purple-500 shadow flex flex-col justify-between">
-        <header>
-          <h2 className="font-bold text-xs tracking-widest font-mono">
-            ES_APPM 252-1
-          </h2>
-          <h1>Integral Calculus</h1>
-        </header>
-
-        <footer className="flex justify-between">
-          <div className="text-sm meta relative">
-            <span className="cursor-pointer">1 – FS</span>
-
-            <div className="bg-black  absolute meta__tooltip text-white border border-gray-800 border-l-4 w-32 p-1 text-xs">
-              <p>Difficulty: 1/5</p>
-              <p>Quarter: Fall/Spring</p>
-            </div>
-          </div>
-
-          <button className="text-xs p-1 bg-purple-600 hover:bg-purple-600 leading-none rounded cursor-pointer">
-            Edit
-          </button>
-        </footer>
+      <div className="container">
+        <AddClassForm />
+        <div className="flex justify-center">
+          <ClassList />
+        </div>
         <Graph></Graph>
       </div>
-    </div>
-  </main>
+    </main>
+  </StoreProvider>
 );
 
 export default Home;
