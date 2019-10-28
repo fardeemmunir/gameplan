15a04099132a3f5cd8f55ebfd3b1dddad79ab1b1
Fardeem Munir <fardeem.munir@erubai.com>
2019-10-27 19:56:33 -0500

    Update the reducer so the add_class action can update classes as well

--- lib/reducer.tsx
+++ lib/reducer.tsx
@@ -14,9 +14,21 @@ function reducer(state = initialState, action: ActionInterface) {
 
   switch (action.type) {
     case "ADD_CLASS":
+      const classList = Array.from(state.classList);
+      const docIndex = classList.findIndex(
+        ({ code }) => code === action.payload.code
+      );
+
+      if (docIndex !== -1)
+        // This class exists so remove the current defintiion
+        // and update it with the new payload
+        classList[docIndex] = action.payload;
+      // classList.splice(docIndex, 1, action.payload);
+      else classList.push(action.payload);
+
       return {
         ...state,
-        classList: state.classList.concat(action.payload)
+        classList
       };
     case "EDIT_CLASS":
       return {
