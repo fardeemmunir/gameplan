04e8ea047cdb2b595cc029fe73e8e746fe6bd634
Fardeem Munir <fardeem.munir@erubai.com>
2019-10-29 00:18:54 -0500

    Add missing key attribute

--- components/Schedule.jsx
+++ components/Schedule.jsx
@@ -15,8 +15,8 @@ const Schedule = () => {
 
       <div className="p-4 w-full rounded bg-white text-black">
         <div className="flex flex-wrap -mx-2">
-          {yearsAndQuarters.map(({ year, quarter }) => (
-            <div className="w-1/3 px-2 mb-2">
+          {yearsAndQuarters.map(({ year, quarter }, i) => (
+            <div key={i} className="w-1/3 px-2 mb-2">
               <div className="border border-gray-200 rounded h-64 p-2">
                 <h1 className="text-sm uppercase font-bold tracking-widest">
                   {year}
