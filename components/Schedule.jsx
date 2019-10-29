64153e869ca0f9414161547d140f27c769628eb3
Fardeem Munir <fardeem.munir@erubai.com>
2019-10-29 00:11:21 -0500

    Create blocks for each school year and quarter

--- components/Schedule.jsx
+++ components/Schedule.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 
+const years = ["Freshman", "Sophmore", "Junior", "Senior"];
+const quarters = ["Fall", "Winter", "Spring"];
+const yearsAndQuarters = years.map(year => {
+  return quarters.map(quarter => ({ year, quarter }));
+});
+
 const Schedule = () => {
   return (
     <div className="w-full mb-10">
       <p className="text-center text-3xl font-bold">Schedule</p>
-      <div className="w-full rounded bg-white p-2 flex flex-wrap">
-        {[1, 2, 3, 4, 5, 6].map(() => (
-          <div className="w-1/3 border border-gray-500 h-12 mr-3"></div>
-        ))}
+
+      <div className="p-4 w-full rounded bg-white text-black">
+        <div className="flex flex-wrap -mx-2">
+          {yearsAndQuarters.flat().map(({ year, quarter }) => (
+            <div className="w-1/3 px-2 mb-2">
+              <div className="border border-gray-200 rounded h-64 p-2">
+                <h1 className="text-sm uppercase font-bold tracking-widest">
+                  {year}
+                </h1>
+                <h1 className="text-3xl leading-none">{quarter}</h1>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
