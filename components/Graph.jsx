f840e02cc97803e05a7f363c99db9612af159aba
Fardeem Munir <fardeem.munir@erubai.com>
2019-11-07 19:21:49 -0600

    Make rounded corners less rounded for the search bar

--- components/Graph.jsx
+++ components/Graph.jsx
@@ -34,7 +34,7 @@ const Graph = () => {
       <div className="w-full text-center mt-8 flex justify-between items-center container">
         <div>
           <input
-            className="bg-transparent focus:outline-none border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+            className="bg-transparent focus:outline-none border border-gray-300 rounded py-2 px-4 block w-full appearance-none leading-normal"
             type="search"
             placeholder="Search Classes"
             value={searchTerm}
