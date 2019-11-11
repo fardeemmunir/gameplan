1c16d87dd5036a84c1d5af9f50bc148698357fe4
Fardeem Munir <fardeem.munir@erubai.com>
2019-11-10 21:41:44 -0600

    Change scheduling data into a more generic data structure

--- components/Schedule.jsx
+++ components/Schedule.jsx
@@ -1,99 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import difficultyToColor from "../lib/difficultyToColor";
 import QuarterCard from "./QuarterCard";
+import Store from "../lib/store";
 
-const years = ["Freshman" /* , "Sophmore", "Junior", "Senior" */];
-const quarters = ["Fall", "Winter", "Spring"];
+const years = ["FRESHMAN" /* , "Sophmore", "Junior", "Senior" */];
+const quarters = ["FALL", "WINTER", "SPRING"];
 const yearsAndQuarters = [];
 
 years.forEach(year => {
   return quarters.forEach(quarter => yearsAndQuarters.push({ year, quarter }));
 });
 
-const classes = [
-  {
-    code: "ES_APPM 252-1",
-    name: "Multivariable Differential Calculus",
-    quarterPref: ["FALL"],
-    difficulty: 3,
-    prereqs: [],
-    index: 0,
-    interest: 1,
-    schedule: "FRESHMAN_FALL"
-  },
-  {
-    code: "COMP_SCI 111",
-    name: "Intro to Programming",
-    prereqs: [],
-    difficulty: 1,
-    quarterPref: ["FALL"],
-    index: 1,
-    interest: 1,
-    schedule: "FRESHMAN_FALL"
-  },
-  {
-    code: "GEN_ENG 205-1",
-    name: "Engineering Analysis 1",
-    prereqs: [],
-    difficulty: 1,
-    quarterPref: ["FALL"],
-    index: 2,
-    interest: 1,
-    schedule: "FRESHMAN_FALL"
-  },
-  {
-    code: "GEN_ENG 205-2",
-    name: "Engineering Analysis 2",
-    prereqs: ["GEN_ENG 205-1"],
-    difficulty: 1,
-    quarterPref: ["WINTER"],
-    index: 3,
-    interest: 1,
-    schedule: "FRESHMAN_WINTER"
-  },
-  {
-    code: "GEN_ENG 205-3",
-    name: "Engineering Analysis 3",
-    prereqs: ["GEN_ENG 205-2"],
-    difficulty: 2,
-    quarterPref: ["SPRING"],
-    index: 4,
-    interest: 1,
-    schedule: "FRESHMAN_SPRING"
-  },
-  {
-    code: "COMP_SCI 212",
-    name: "Mathematical Foundations of Comp Science",
-    prereqs: ["COMP_SCI 111"],
-    difficulty: 4,
-    quarterPref: ["FALL", "WINTER", "SPRING"],
-    index: 5,
-    interest: 1,
-    schedule: "FRESHMAN_WINTER"
-  },
-  {
-    code: "DSGN 106-1",
-    name: "DTC 1",
-    prereqs: [],
-    difficulty: 2,
-    quarterPref: ["FALL"],
-    index: 6,
-    interest: 1,
-    schedule: "FRESHMAN_FALL"
-  },
-  {
-    code: "DSGN 106-2",
-    name: "DTC 2",
-    prereqs: ["DSGN 106-1"],
-    difficulty: 2,
-    quarterPref: ["SPRING"],
-    index: 7,
-    interest: 1,
-    schedule: "FRESHMAN_SPRING"
-  }
-];
+const schedule = {
+  FRESHMAN_FALL: [
+    "ES_APPM 252-1",
+    "COMP_SCI 111",
+    "GEN_ENG 205-1",
+    "DSGN 106-1"
+  ],
+  FRESHMAN_WINTER: ["GEN_ENG 205-2", "COMP_SCI 212"],
+  FRESHMAN_SPRING: ["GEN_ENG 205-3", "DSGN 106-2"]
+};
 
 const Schedule = () => {
+  const { classList } = useContext(Store);
+
   return (
     <div className="w-full mb-10">
       <p className="text-center text-gigantic opacity-25 font-bold">Schedule</p>
@@ -105,10 +36,8 @@ const Schedule = () => {
               key={i}
               year={year}
               quarter={quarter}
-              classes={classes.filter(
-                classInfo =>
-                  classInfo.schedule ===
-                  year.toUpperCase() + "_" + quarter.toUpperCase()
+              classes={classList.filter(({ code }) =>
+                schedule[year + "_" + quarter].includes(code)
               )}
             />
           ))}
