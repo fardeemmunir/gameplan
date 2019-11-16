d6aa1de39fcfcf4ba0b91448e26cf83620a44665
Fardeem Munir <fardeem.munir@erubai.com>
2019-11-15 20:42:07 -0600

    Add links to the footer yay!

--- pages/index.jsx
+++ pages/index.jsx
@@ -31,7 +31,10 @@ const Home = () => {
 
         <footer className="text-white opacity-50 pb-8 pt-4 text-sm container flex justify-between">
           <p className="text-right tracking-tight">
-            Build using D3, Tailwind.css and React and Friends
+            Build using <a href="https://d3js.org/">D3</a>,{" "}
+            <a href="https://tailwindcss.com/">Tailwind.css</a> and{" "}
+            <a href="https://reactjs.org/">React</a> &amp;{" "}
+            <a href="https://nextjs.org/">Friends</a>
           </p>
           <p className="text-right tracking-tight">Made by Fardeem Munir</p>
         </footer>
