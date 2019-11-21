489653edbd0c7f18b47a00a89eefc9fb12ed6339
Fardeem Munir <fardeem.munir@erubai.com>
2019-11-21 01:19:45 -0600

    Add some test cases to the test runner

--- lib/scheduler.test.tsx
+++ lib/scheduler.test.tsx
@@ -1,5 +1,30 @@
 import scheduler from "./scheduler";
 
 test("hello world", () => {
-  expect(1 + 1).toEqual(2);
+  const classList = [
+    {
+      code: "A",
+      prereqs: [],
+      quarterPref: "FALL"
+    },
+    {
+      code: "B",
+      prereqs: ["A"],
+      quarterPref: "WINTER"
+    },
+    {
+      code: "C",
+      prereqs: ["C"],
+      quarterPref: "FALL"
+    }
+  ];
+
+  const output = {
+    FALL_0: ["A"],
+    WINTER_0: ["B"],
+    SPRING_0: [],
+    FALL_1: ["C"]
+  };
+
+  expect(scheduler(classList)).toBe(output);
 });
