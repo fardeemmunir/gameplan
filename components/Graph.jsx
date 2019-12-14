b7e69a1249028a8290291f6d15c904df6e334c63
Fardeem Munir <fardeem.munir@erubai.com>
2019-12-14 00:23:24 -0600

    Add a list view for the classes

--- components/Graph.jsx
+++ components/Graph.jsx
@@ -4,6 +4,7 @@ import Store, { ClassInfoInterface } from "../lib/store";
 import makeLinks from "../lib/makeLinks";
 import NetworkGraph from "./NetworkGraph";
 import Share from "./Share";
+import ListView from "./Listview";
 
 const Graph = () => {
   const { classList: nodes } = useContext(Store);
@@ -14,6 +15,7 @@ const Graph = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [linkDistance, setLinkDistance] = useState(150);
   const [nodeDistance, setNodeDistance] = useState(900);
+  const [showGraph, setShowGraph] = useState(true);
 
   /**
    * @param {ClassInfoInterface[]} nodes
@@ -34,8 +36,19 @@ const Graph = () => {
 
   return (
     <>
-      <div className="w-full text-center mt-8 flex justify-between items-center container">
+      <div className="w-full text-center mt-8 mb-4 flex justify-between items-center container">
         <div className="flex items-center">
+          <style jsx>{`
+            .toggle-btn {
+              min-width: 8rem;
+            }
+          `}</style>
+          <button
+            onClick={() => setShowGraph(!showGraph)}
+            className="block toggle-btn p-2 mr-4 bg-blue-600 rounded form__submit mb-0 whitespace-no-wrap"
+          >
+            {!showGraph ? "Graph View" : "List View"}
+          </button>
           <input
             className="bg-transparent focus:outline-none border border-gray-300 rounded py-2 px-4 mr-4 block w-full appearance-none leading-normal"
             type="search"
@@ -43,25 +56,26 @@ const Graph = () => {
             value={searchTerm}
             onChange={e => setSearchTerm(e.target.value)}
           />
-          <div className="text-left mr-4">
+
+          <div className={"text-left mr-4 " + (!showGraph && "invisible")}>
             <h1 className="text-sm">Link Distance</h1>
             <input
               type="range"
               className="range"
               min="100"
               max="300"
-              value={linkDistance}
+              defaultValue={linkDistance}
               onChange={e => setLinkDistance(Number(e.target.value))}
             />
           </div>
-          <div className="text-left">
+          <div className={"text-left " + (!showGraph && "invisible")}>
             <h1 className="text-sm">Node Distance</h1>
             <input
               type="range"
               className="range"
               min="500"
               max="1500"
-              value={nodeDistance}
+              defaultValue={nodeDistance}
               onChange={e => setNodeDistance(Number(e.target.value))}
             />
           </div>
@@ -83,13 +97,32 @@ const Graph = () => {
           <Share />
         </div>
       </div>
-      <NetworkGraph
-        nodes={mapNodesToSearchTerm(nodes)}
-        links={links}
-        linkDistance={linkDistance}
-        nodeDistance={nodeDistance}
-        isSearching={!!searchTerm}
-      />
+
+      {showGraph ? (
+        <NetworkGraph
+          nodes={mapNodesToSearchTerm(nodes)}
+          links={links}
+          linkDistance={linkDistance}
+          nodeDistance={nodeDistance}
+          isSearching={!!searchTerm}
+        />
+      ) : (
+        <ListView
+          classList={mapNodesToSearchTerm(nodes)
+            .filter(
+              // @ts-ignore
+              ({ isSearched }) => {
+                if (searchTerm.length > 0) return isSearched;
+                return true;
+              }
+            )
+            .map(info => ({
+              ...info,
+              prereqs: info.prereqs.join(", "),
+              quarterPref: info.prereqs.join(", ")
+            }))}
+        />
+      )}
     </>
   );
 };
