414d80f69f64f25689e7b285a295daf806f5ca2d
Fardeem Munir <fardeem.munir@erubai.com>
2020-01-14 20:56:36 -0600

    Minify and remove unused css

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,17 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
-  plugins: [require("tailwindcss"), require("postcss-preset-env")]
+  plugins: [
+    require("tailwindcss"),
+    require("postcss-preset-env"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+    require("autoprefixer"),
+    require("cssnano")
+  ]
 };
