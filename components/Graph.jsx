70a39dfa92da1d6645a6535c9d73cfc3c2df13ef
Fardeem Munir <fardeem.munir@erubai.com>
2020-01-19 01:27:29 -0600

    Fix random bug

--- components/Graph.jsx
+++ components/Graph.jsx
@@ -126,18 +126,24 @@ const Graph = () => {
         </div>
       </div>
 
-      {nodes.length === 0 ? (
-        <div className=" my-48">
-          <img className="w-1/4 mx-auto" src="/plan.png" alt="" />
-          <p className="text-xl tracking-wider text-center opacity-50">
-            Add some classes
-          </p>
-        </div>
-      ) : showGraph ? (
-        <GraphView />
-      ) : (
-        <ListViewWithOptions />
-      )}
+      {(() => {
+        if (nodes.length === 0) {
+          return (
+            <div className="my-48">
+              <img className="w-1/4 mx-auto" src="/plan.png" alt="" />
+              <p className="text-xl tracking-wider text-center opacity-50">
+                Add some classes
+              </p>
+            </div>
+          );
+        } else {
+          if (showGraph) {
+            return <GraphView />;
+          } else {
+            return <ListViewWithOptions />;
+          }
+        }
+      })()}
     </>
   );
 };
