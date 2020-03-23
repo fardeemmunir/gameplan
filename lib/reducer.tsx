59ed36778b54cb8740d081a9d35dfd6c596929fa
Fardeem Munir <fardeem.munir@erubai.com>
2020-03-23 18:40:32 -0400

    Rewrite reducer using the redux toolkit

--- lib/reducer.tsx
+++ lib/reducer.tsx
@@ -1,91 +1,111 @@
-import { initialState, ClassInfoInterface, ScheduleInterface } from "./store";
+import { v4 as uuidv4 } from "uuid";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-interface ActionInterface {
-  type:
-    | "ADD_CLASS"
-    | "EDIT_CLASS"
-    | "FINISH_EDITING_CLASS"
-    | "REMOVE_CLASS"
-    | "UPDATE_SCHEDULE";
-  payload: ClassInfoInterface & EditClassPayload & UpdatedSchedulePayload;
-}
+//
+// Types
+//
+export type Class = {
+  id: string;
+  code: string;
+  name: string;
+  prereqs: string[];
+  difficulty: number;
+  interest: number;
+  quarterPref: string[];
+};
 
-interface EditClassPayload {
-  classCode: string;
-}
+export type Schedule = {
+  [key: string]: string[];
+};
 
-interface UpdatedSchedulePayload {
-  updatedSchedule: ScheduleInterface;
-}
+export type EditClass = string;
 
-function reducer(state = initialState, action: ActionInterface) {
-  switch (action.type) {
-    case "ADD_CLASS": {
-      const classList = Array.from(state.classList);
-      const docIndex = classList.findIndex(
-        ({ code }) => code === action.payload.code
+export type Store = {
+  classList: Class[];
+  schedule: {
+    data: Schedule;
+    locks: string[];
+  };
+  editClass: EditClass;
+};
+
+//
+// Reducer
+//
+export const initialState: Store = {
+  classList: [],
+  schedule: {
+    data: {},
+    locks: []
+  },
+  editClass: ""
+};
+
+const store = createSlice({
+  name: "store",
+  initialState,
+  reducers: {
+    addOrUpdateClass(state, action: PayloadAction<Class>) {
+      const { classList } = state;
+      const classToBeAdded = action.payload;
+      const classIndexIfExists = classList.findIndex(
+        ({ id }) => id === classToBeAdded.id
       );
 
-      // Find all the classes that don't exist in our list
-      // Add them to the list
-      action.payload.prereqs
-        .filter(code => {
-          return !classList.map(({ code }) => code).includes(code);
-        })
-        .forEach(code => {
-          classList.push({
-            code,
-            name: "",
-            interest: 0,
-            quarterPref: [],
-            difficulty: 0,
-            prereqs: []
-          });
+      if (classIndexIfExists === -1) {
+        classList.push(classToBeAdded);
+      } else {
+        classList[classIndexIfExists] = classToBeAdded;
+      }
+
+      for (let i = 0; i < classToBeAdded.prereqs.length; i++) {
+        const prereqIdOrCode = classToBeAdded.prereqs[i];
+
+        if (classList.find(({ id }) => id === prereqIdOrCode)) continue;
+
+        const id = uuidv4();
+        classToBeAdded.prereqs[i] = id;
+        classList.push({
+          id,
+          name: "",
+          prereqs: [],
+          interest: 0,
+          code: prereqIdOrCode,
+          difficulty: 0,
+          quarterPref: []
         });
+      }
+    },
 
-      if (docIndex !== -1)
-        // This class exists so remove the current defintiion
-        // and update it with the new payload
-        classList[docIndex] = action.payload;
-      else classList.push(action.payload);
+    removeClass(state, action: PayloadAction<string>) {
+      const classId = action.payload;
+      const { classList, schedule } = state;
+      const classIndex = classList.findIndex(({ id }) => id === classId);
 
-      return {
-        ...state,
-        classList
-      };
-    }
-    case "EDIT_CLASS":
-      return {
-        ...state,
-        editClass: action.payload.classCode
-      };
-    case "FINISH_EDITING_CLASS":
-      return {
-        ...state,
-        editClass: ""
-      };
-    case "REMOVE_CLASS": {
-      const classList = Array.from(state.classList);
-      const index = classList.findIndex(
-        ({ code }) => code === action.payload.classCode
-      );
+      classList.splice(classIndex, 1);
 
-      classList.splice(index, 1);
-      return {
-        ...state,
-        classList,
-        editClass: ""
-      };
+      for (let quarter in schedule) {
+        const classIdsInQuarter = schedule.data[quarter];
+        const classIndexIfExists = classIdsInQuarter.findIndex(
+          id => id === classId
+        );
+
+        if (classIndexIfExists === -1) {
+          continue;
+        } else {
+          classIdsInQuarter.splice(classIndexIfExists, 1);
+        }
+      }
+
+      state.editClass = "";
+    },
+
+    setClassToEdit(state, action: PayloadAction<string>) {
+      state.editClass = action.payload;
     }
-    case "UPDATE_SCHEDULE": {
-      return {
-        ...state,
-        schedule: action.payload.updatedSchedule
-      };
-    }
-    default:
-      return state;
   }
-}
+});
 
-export default reducer;
+export default store.reducer;
+
+export const { addOrUpdateClass, removeClass, setClassToEdit } = store.actions;
