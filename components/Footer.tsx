e655b589d06442d26323bb7d26b1c1dd4b84d474
Fardeem Munir <fardeem.munir@erubai.com>
2020-03-29 20:56:39 -0400

    Add faunadb to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,6 +4,7 @@ const Footer = () => (
   <footer className="text-gray-500 pb-8 pt-4 text-sm container flex justify-between">
     <p className="text-right tracking-tight">
       Build using <a href="https://d3js.org/">D3</a>,{" "}
+      <a href="https://fauna.com/">Fauna DB</a>,{" "}
       <a href="https://tailwindcss.com/">Tailwind.css</a> and{" "}
       <a href="https://reactjs.org/">React</a> &amp;{" "}
       <a href="https://nextjs.org/">Friends</a>
