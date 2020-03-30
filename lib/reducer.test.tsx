77802d7817b8cb44187f204d8646443438897ef1
Fardeem Munir <fardeem.munir@erubai.com>
2020-03-29 21:31:27 -0400

    More tests

--- lib/reducer.test.tsx
+++ lib/reducer.test.tsx
@@ -1,4 +1,9 @@
-import reducer, { initialState, addOrUpdateClass } from "./reducer";
+import reducer, {
+  initialState,
+  addOrUpdateClass,
+  removeClass,
+  setClassToEdit
+} from "./reducer";
 
 const classToAdd = {
   id: "123",
@@ -37,7 +42,6 @@ test("Class is updated appropriately", () => {
 test("If class has a new prereq, it gets automatically added", () => {
   classToAdd.prereqs.push("some_class");
   const output = reducer(initialState, addOrUpdateClass(classToAdd));
-
   expect(output).toStrictEqual({
     ...initialState,
     classList: [
@@ -52,5 +56,66 @@ test("If class has a new prereq, it gets automatically added", () => {
         quarterPref: []
       }
     ]
+  });
+});
+
+test("Class is removed from list", () => {
+  const state = {
+    ...initialState,
+    classList: [classToAdd]
+  };
+
+  expect(reducer(state, removeClass(classToAdd.id))).toStrictEqual(
+    initialState
+  );
+});
+
+test("Class is removed from schedule if it exists", () => {
+  const state = {
+    ...initialState,
+    classList: [classToAdd],
+    schedule: {
+      locks: [],
+      data: {
+        FALL_0: [classToAdd.id]
+      }
+    }
+  };
+
+  expect(reducer(state, removeClass(classToAdd.id))).toStrictEqual({
+    ...initialState,
+    schedule: {
+      locks: [],
+      data: {
+        FALL_0: []
+      }
+    }
+  });
+});
+
+test("If a class exists, it can be edited", () => {
+  const state = {
+    ...initialState,
+    classList: [classToAdd]
+  };
+  expect(reducer(state, setClassToEdit(classToAdd.id))).toStrictEqual({
+    ...state,
+    editClass: classToAdd.id
+  });
+});
+
+test("If a class doesn't exist, then it cannot be edited", () => {
+  const state = {
+    ...initialState
+  };
+  expect(reducer(state, setClassToEdit(classToAdd.id))).toStrictEqual({
+    ...state
+  });
+});
+
+test("Class editing can be empty", () => {
+  expect(reducer(initialState, setClassToEdit(""))).toStrictEqual({
+    ...initialState,
+    editClass: ""
   });
 });
--- lib/reducer.tsx
+++ lib/reducer.tsx
@@ -143,6 +143,12 @@ const storeReducer = function(state: Store, action: StoreActions): Store {
     }
 
     case "SET_CLASS_TO_EDIT": {
+      if (
+        action.payload !== "" &&
+        !state.classList.find(({ id }) => id === action.payload)
+      )
+        return state;
+
       return {
         ...state,
         editClass: action.payload
